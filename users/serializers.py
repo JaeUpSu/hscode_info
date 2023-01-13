a0f4f7f8acc21de38360d62030e23481f1bf9f81
jaeupsu <hyeongs2323@gmail.com>
2023-01-13 17:12:56 +0900

    feat: users serializers add

---
+++ users/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+from .models import User
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id","name","tel", "email","request")
+        
